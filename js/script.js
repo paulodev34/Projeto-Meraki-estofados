07eb5f963920a617f6dc1c5ff84e8a3614d96a66
Billy paulinho <paulo_neal@hotmail.com>
2023-03-06 15:34:09 -0300

    Slide de fotos e função JavaScript para slide

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+/*function previousSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+function gotoSlide(n) {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = n;
+  slides[currentSlide].classList.add("active");
+}
+*/
